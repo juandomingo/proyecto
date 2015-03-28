d2693e7fd74da690ad288d970cbb97450cb2f5f0
tinoliam <tinoliam@gmail.com>
2015-03-28 00:14:36 -0300

    jk

--- final/app/Controller/AppController.php
+++ final/app/Controller/AppController.php
@@ -47,6 +47,7 @@ class AppController extends Controller {
 	}
 
 	public function beforeFilter(){
+		$this->Auth->autoRedirect = false;
 		$this->Auth->allow('home','login','logout');
 		$this->set('logged_in',$this->Auth->loggedIn());
 		$this->set('current_user',$this->Auth->user());
